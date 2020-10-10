bf4d70e0009ea11fafceb862705ab132b95580cc
Swapnil Patil <swapnilpatil2607@gmail.com>
2020-10-10 17:05:15 +0530

    js changes

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ window.addEventListener("load",function(){
 function login(){
     event.preventDefault();
     var form=new FormData(event.target);
-    var email=form.get("L-email");
-    var password=form.get("L-password");
+    var email=form.get("l-email");
+    var password=form.get("l-password");
     var p=document.createElement("p");
     var body=document.createElement("body");
     
@@ -29,20 +29,15 @@ function register(){
     event.preventDefault();
     var form=new FormData(event.target);
     var name=form.get("name");
-    var email=form.get("R-email");
-    var password=form.get("R-password");
+    var email=form.get("r-email");
+    var password=form.get("r-password");
     var p=document.createElement("p");
     var body=document.createElement("body");
-    if(name.length>3 && password.length>5){
+    console.log(name);
+    console.log(email);
         var data={name:name,email:email,password:password};
         data=JSON.stringify(data);
         localStorage(email,data);
-    }
-    else{
-        p.textContent=`Name should be mininmum 4 character
-                      password should be minimum 6 characters`
-        body.append(p);
-    }
 }
 function reg(){
 var regdiv=document.getElementById("register-form");
