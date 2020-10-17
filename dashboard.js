297ff963af67c03c744fe3a0e06ec12081372a67
Swapnil Patil <swapnilpatil2607@gmail.com>
2020-10-17 11:16:02 +0530

    dashboard logic-3 active user problem resolved

--- dashboard.js
+++ dashboard.js
@@ -2,5 +2,10 @@ window.addEventListener("load",function(){
 if(!localStorage.active){
 location.assign("index.html");
 }
-
+var logout=document.getElementById("log-out");
+logout.addEventListener("click",clear);
 });
+function clear(){
+    localStorage.setItem("active","");
+    location.assign("index.html");
+}
