41b4c8fe73ced9b5f168fa4c931a6ec376248e57
Swapnil Patil <swapnilpatil2607@gmail.com>
2020-10-17 11:07:26 +0530

    dashboard logic-1

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,5 @@
 window.addEventListener("load",function(){
-var form=document.querySelector("form");
-form.addEventListener("submit",update);
-});
-function update(){
-    event.preventDefault();
-    var form=new Formdata(event.target);
-    
+if(!localStorage.active){
+location.assign("index.html");
 }
+});
