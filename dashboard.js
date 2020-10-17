f5083943fceb6cd79b33b5f0f78a4d606283cbd0
Swapnil Patil <swapnilpatil2607@gmail.com>
2020-10-17 13:52:16 +0530

    onload transaction

--- dashboard.js
+++ dashboard.js
@@ -6,6 +6,46 @@ var logout=document.getElementById("log-out");
 logout.addEventListener("click",clear);
 var add=document.getElementById("add-trans");
 add.addEventListener("click",transinput);
+
+var len=JSON.parse(localStorage.user);
+var income=0;
+for(var a=0;a<len.length;a++){
+   
+    if(len[a].email==localStorage.active){
+      
+
+        for(var b=0;b<len[a].length;b++){
+            console.log(len[a].transaction[b].type);
+        }
+
+    }
+
+}
+
+for(var k=0;k<len.length;k++){
+if(len[k].email==localStorage.active){
+var table=document.querySelector("tbody");
+for(var m=0;m<5;m++){
+    var tr=document.createElement("tr");
+    var title=document.createElement("td");
+    title.textContent=len[k].transaction[m].title;
+
+    var type=document.createElement("td");
+    type.textContent=len[k].transaction[m].type;
+
+    var amount=document.createElement("td");
+    amount.textContent=len[k].transaction[m].amount;
+
+    var timestamp=document.createElement("td");
+    timestamp.textContent=len[k].transaction[m].timestamp;
+
+    tr.append(title,type,amount,timestamp);
+    table.append(tr);
+}
+   
+}
+}
+console.log(table);
 });
 function clear(){
     localStorage.setItem("active","");
@@ -25,6 +65,7 @@ function gettransaction(){
     var amount =modal.get("amount");
     for(var i=0;i<len.length;i++){
         if(len[i].email==localStorage.active){    
+    
           len[i].transaction.push({title:title,type:type,amount:amount,timestamp:Date()});
           localStorage.setItem("user",JSON.stringify(len));
           console.log(len[i].transaction);
