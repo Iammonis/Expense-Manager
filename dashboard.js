9b763c9d7e7def7c483cd935708760d4592537da
Swapnil Patil <swapnilpatil2607@gmail.com>
2020-10-17 12:23:40 +0530

    transaction push resolved

--- dashboard.js
+++ dashboard.js
@@ -14,4 +14,21 @@ function clear(){
 function transinput(){
     var modal=document.getElementById("modal");
     modal.style.display="block";
+    modal.addEventListener("submit",gettransaction);
+}
+function gettransaction(){
+    event.preventDefault();
+    var len=JSON.parse(localStorage.user);
+    var modal=new FormData(event.target);
+    var title=modal.get("title");
+    var type=modal.get("type");
+    var amount =modal.get("amount");
+    for(var i=0;i<len.length;i++){
+        if(len[i].email==localStorage.active){    
+          len[i].transaction.push({title:title,type:type,amount:amount,timestamp:Date()});
+          localStorage.setItem("user",JSON.stringify(len));
+          console.log(len[i].transaction);
+        }
+    }
+    
 }
