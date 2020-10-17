ce2ce96e2cc71aac3a1fb88b50d5578516130474
Swapnil Patil <swapnilpatil2607@gmail.com>
2020-10-17 11:29:08 +0530

    dashboard updates

--- dashboard.js
+++ dashboard.js
@@ -4,8 +4,14 @@ location.assign("index.html");
 }
 var logout=document.getElementById("log-out");
 logout.addEventListener("click",clear);
+var modal=document.getElementById("modal");
+modal.addEventListener("click",transinput);
 });
 function clear(){
     localStorage.setItem("active","");
     location.assign("index.html");
+}
+function transinput(){
+    var modal=document.getElementById("modal");
+    modal.Style.display="block";
 }
